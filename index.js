ea763b2845cf9880a0ba5feadd25a48507cc6589
gibz tajada <gibz1432@gmail.com>
2022-02-08 07:26:35 +0800

    5th commit

--- index.js
+++ index.js
@@ -34,19 +34,19 @@ app.use('/users', userRoutes);
 app.use('/courses', courseRoutes);
 
 //error handlers
-app.use((req, res, next) => {
-	const error = new Error('Not found');
-	error.status = 404;
-	next(error);
-})
+// app.use((req, res, next) => {
+// 	const error = new Error('Not found');
+// 	error.status = 404;
+// 	next(error);
+// })
 
-app.use((error, req, res, next) => {
-	res.status(error.status || 500);
-	res.send(`Error: ${error.message}`);
-})
+// app.use((error, req, res, next) => {
+// 	res.status(error.status || 500);
+// 	res.send(`Error: ${error.message}`);
+// })
 
 app.get('/', (req, res) => {
-	res.send('Hosted');
+	res.send('Hosted on heroku');
 })
 
 app.listen(port, () => console.log(`Server running at port ${port}`))
